0d7b7e95c43063fc33dd886b75f2c5a80fed1458
jiriklepl <jiriklepl@seznam.cz>
2023-07-19 15:31:24 +0200

    add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create the build directory
+cmake -E make_directory build || exit 1
+cd build || exit 1
+
+# Configure the build
+cmake -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS="-O3 -mtune=native -DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DLARGE_DATASET -DDATA_TYPE_IS_DOUBLE -D_POSIX_C_SOURCE=200809L " \
+    .. \
+    || exit 1
+
+# Build the project
+NPROC=$(nproc)
+cmake --build . --config Release -j"$NPROC" || exit 1
