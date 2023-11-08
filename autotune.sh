d4ce615c09e20828014123944ae8df0d237b31bc
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2023-11-08 03:49:45 +0100

    improve scripts

--- autotune.sh
+++ autotune.sh
@@ -4,11 +4,13 @@ NUM_RUNS=100
 
 mkdir -p build
 
+SET_CONFIG="-DEXTRALARGE_DATASET -DDATA_TYPE_IS_FLOAT"
+
 (
 	cd build
 
 	# Build autotuners
-	cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_CXX_FLAGS="-DLARGE_DATASET -DDATA_TYPE_IS_DOUBLE "
+	cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_CXX_FLAGS="$SET_CONFIG "
 	cmake --build . -j"$(nproc)"
 )
 
@@ -30,8 +32,10 @@ for file in build/*_autotune; do
 		# transform the configuration into a list of defines
 		config=$(grep -oE '"\w+"\s*:\s*[^,}]+' ../build/mmm_final_config.json | sed -E 's/"|://g' | awk '{printf("%s", "-DNOARR_PARAMETER_VALUE_" $1 "=" $2 " ")}END{print ""}')
 
+		echo "$config" > "$filename-config"
+
 		# build the autotuned version of the program
-		cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_CXX_FLAGS="-DLARGE_DATASET -DDATA_TYPE_IS_DOUBLE -DNOARR_PASS_BY_DEFINE $config"
+		cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_CXX_FLAGS="$SET_CONFIG -DNOARR_PASS_BY_DEFINE $config"
 		cmake --build . -j"$(nproc)" -t "$(echo "$filename" | sed -E 's/_autotune//')"
 	)
 done
--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # Create the build directory
-cmake -E make_directory build || exit 1
-cd build || exit 1
+cmake -E make_directory build
+cd build
 
 # Configure the build
 cmake -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_FLAGS="-DLARGE_DATASET -DDATA_TYPE_IS_DOUBLE " \
-    .. \
-    || exit 1
+    ..
 
 # Build the project
 NPROC=$(nproc)
-cmake --build . --config Release -j"$NPROC" || exit 1
+cmake --build . --config Release -j"$NPROC"
